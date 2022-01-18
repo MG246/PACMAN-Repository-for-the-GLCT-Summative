e2ff6b841f410919da906c853b960c49ef23c7ed
Mr M <97130156+MG246@users.noreply.github.com>
2022-01-18 19:25:15 +0100

    Update Pacman_enemy.py

--- Pacman/Pacman_enemy.py
+++ Pacman/Pacman_enemy.py
@@ -1,13 +1,14 @@
-from colorsys import rgb_to_yiq
+
+from random import random
 import pygame
-from PacmanSettings import *
+from settings import *
 vec = pygame.math.Vector2
 
 
 class Enemy:
-    def _init_(self,app, pos, number):
+    def __init__(self,app, pos, number):
         self.app = app
-        self.grid = pos
+        self.grid_pos = pos
         self.pix_pos = self.get_pix_pos()
         self.radius = self.app.cell_width//2.3
         self.number = number 
@@ -27,9 +28,9 @@ class Enemy:
             self.move()
             
         # Setting grid position in reference to pix position
-        self.grid_pos[0] = (self.pix_pos[0]-TOP_BOTTOM_BUFFER +
+        self.grid_pos[0] = (self.pix_pos[0]-Top_Bottom_Buffer +
                             self.app.cell_width//2)//self.app.cell_width+1
-        self.grid_pos[1] = (self.pix_pos[1]-TOP_BOTTOM_BUFFER +
+        self.grid_pos[1] = (self.pix_pos[1]-Top_Bottom_Buffer +
                             self.app.cell_height//2)//self.app.cell_height+1
 
 
@@ -38,15 +39,15 @@ class Enemy:
             pygame.draw.circle(self.app.screen, self.colour, (int(self.pix_pos.x), int(self.pix_pos.y)), self.radius)
     
     def set_speed(self):
-         if self.personality in ["speedy", "scared"]:
+        if self.personality in ["speedy", "scared"]:
             speed = 2
         else:
             speed = 1
         return speed
     
     def set_target (self):
-        if self.personality == "speedy" or sel.personality == "slow":
-            return vec self.app.player.grid_pos
+        if self.personality == "speedy" or self.personality == "slow":
+            return vec (self.app.player.grid_pos)
         else:
             if self.app.player.grid_pos.x > COLS//2 and self.app.player.grid_pos.y > ROWS//2:
                 return vec(1, 1)
@@ -59,10 +60,10 @@ class Enemy:
                 return vec(COLS-2, ROWS-2)
     
     def time_to_move(self):
-        if int(self.pix_pos.x+TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
+        if int(self.pix_pos.x+Top_Bottom_Buffer//2) % self.app.cell_width == 0:
             if self.direction == vec(1, 0) or self.direction == vec(-1, 0) or self.direction == vec(0, 0):
                 return True
-        if int(self.pix_pos.y+TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
+        if int(self.pix_pos.y+Top_Bottom_Buffer//2) % self.app.cell_height == 0:
             if self.direction == vec(0, 1) or self.direction == vec(0, -1) or self.direction == vec(0, 0):
                 return True
         return False
@@ -88,10 +89,10 @@ class Enemy:
         return path[1]
     
     def BFS(self, start, target):
-        grid = [[0 for x in range(28) for x in range(30)]]
+        grid = [[0 for x in range(28)] for x in range(30)]
         for cell in self.app.walls:
             if cell.x < 28 and cell.y < 30:
-                grid[int(cell.y)][int(cell.x)]
+                grid[int(cell.y)][int(cell.x)] = 1
         queue = [start]
         path = []
         visited = []
@@ -106,11 +107,11 @@ class Enemy:
                 for neighbour in neighbours:
                     if neighbour[0]+current[0] >= 0 and neighbour[0] + current[0] < len(grid[0]):
                         if neighbour[1]+current[1] >= 0 and neighbour[1] + current[1] < len(grid):
-                            next_cell = [neighboour[0] + current[0], neighbour[1] + current[1]]
+                            next_cell = [neighbour[0] + current[0], neighbour[1] + current[1]]
                             if next_cell not in visited:
-                                if grid[next_cell[1][next_cell[0]] != 1
-                                        queue.append(next_cell)
-                                        path.append({"Current": current, "Next": next_cell})
+                                if grid[next_cell[1]][next_cell[0]] != 1:
+                                    queue.append(next_cell)
+                                    path.append({"Current": current, "Next": next_cell})
         
         shortest = [target]
         while target != start:
@@ -120,9 +121,24 @@ class Enemy:
                    shortest.insert(0, step["Current"])                    
         return shortest              
                    
+    def get_random_direction(self):
+        while True:
+            number = random.randint(-2, 1)
+            if number == -2:
+                x_dir, y_dir = 1, 0
+            elif number == -1:
+                x_dir, y_dir = 0, 1
+            elif number == 0:
+                x_dir, y_dir = -1, 0
+            else:
+                x_dir, y_dir = 0, -1
+            next_pos = vec(self.grid_pos.x + x_dir, self.grid_pos.y + y_dir)
+            if next_pos not in self.app.walls:
+                break
+        return vec(x_dir, y_dir)
 
     def get_pix_pos(self):
-        return vec((self.grid_pos.x*self.app.cell_width) + Top_Bottom_Buffer//2 +self.app.cell_width//2, (self.grid_pos.y*self.app.cell_height) +Top_Bottom_Buffer//2 + self.app.cell_heigth//2)
+        return vec((self.grid_pos.x*self.app.cell_width) + Top_Bottom_Buffer//2 +self.app.cell_width//2, (self.grid_pos.y*self.app.cell_height) +Top_Bottom_Buffer//2 + self.app.cell_height//2)
 
     def set_colour(self):
         if self.number == 0:
@@ -140,7 +156,7 @@ class Enemy:
             return "speedy"
         elif self.number == 1:
             return "slow"
-        if self.number == 2:
+        elif self.number == 2:
             return "random"
         else:
             return "scared"
